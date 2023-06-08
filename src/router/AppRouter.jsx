b3ef215bded0927e4c3ce690681af7d639587e16
Rasec Camacho <rasec.camacho19@gmail.com>
2023-06-07 22:39:53 -0700

    navbar

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,19 +1,20 @@
-import { Navigate, Route, Routes } from "react-router-dom"
-import { MarvelPages, DcPage } from "../heroes/pages"
-import { LoginPage } from "../auth/pages"
-
+import { Navigate, Route, Routes } from "react-router-dom";
+import { MarvelPages, DcPage } from "../heroes/pages";
+import { LoginPage } from "../auth/pages";
+import { Navbar } from "../ui/components";
 
 export const AppRouter = () => {
   return (
     <>
-        <Routes>
-            <Route path="marvel" element={ <MarvelPages /> }></Route>
-            <Route path="dc" element={ <DcPage /> }></Route>
-            
-            <Route path="login" element={ <LoginPage /> }></Route>
+      <Navbar />
+      <Routes>
+        <Route path="marvel" element={<MarvelPages />}></Route>
+        <Route path="dc" element={<DcPage />}></Route>
 
-            <Route path="/" element={ <Navigate to={"/marvel"} /> }></Route>
-        </Routes>
+        <Route path="login" element={<LoginPage />}></Route>
+
+        <Route path="/" element={<Navigate to={"/marvel"} />}></Route>
+      </Routes>
     </>
-  )
-}
+  );
+};

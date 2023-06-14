e27ce0f79bed393a8e2dc68316e252d42811a5b2
Rasec Camacho <rasec.camacho19@gmail.com>
2023-06-13 19:56:51 -0700

    add context and provider

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,9 +1,12 @@
-import { AppRouter } from "./router/AppRouter"
+import { AuthProvider } from "./auth/context/AuthProvider";
+import { AppRouter } from "./router/AppRouter";
 
 export const HeroesApp = () => {
   return (
     <>
+      <AuthProvider>
         <AppRouter />
+      </AuthProvider>
     </>
-  )
-}
+  );
+};

4ac55e8f713c708284f3c5050970ca56e2d6cce2
Rasec Camacho <rasec.camacho19@gmail.com>
2023-08-08 15:01:11 -0700

    login user

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -1,19 +1,27 @@
 import { useReducer } from "react";
 
-import { AuthContext } from "./AuthContext"
-import { authReducer } from "./authReducer"
+import { AuthContext } from "./AuthContext";
+import { authReducer } from "./authReducer";
+import { types } from "../types/types";
 
 const initialState = {
-    logged: false,
-}
+  logged: false,
+};
 
 export const AuthProvider = ({ children }) => {
+  const [authState, dispatch] = useReducer(authReducer, initialState);
 
-    const [ authState, dispatch ] = useReducer( authReducer, initialState );
+  const login = ( name = '' ) => {
+    const action = {
+      type: types.login,
+      payload: { 
+        id: 'ABC',
+        name 
+      },
+    }
+    
+    dispatch(action);
+  }
 
-  return (
-    <AuthContext.Provider value={{ }}>
-        { children }
-    </AuthContext.Provider>
-  )
-}
+  return <AuthContext.Provider value={{ ...authState, login }}>{children}</AuthContext.Provider>;
+};

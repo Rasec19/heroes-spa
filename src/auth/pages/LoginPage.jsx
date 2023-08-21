c1036c094404b5d4d2b5f3a80691c70434af0048
Rasec Camacho <rasec.camacho19@gmail.com>
2023-08-21 11:33:21 -0700

    rutas privas y publicasa

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -9,9 +9,11 @@ export const LoginPage = () => {
 
   const onLogin = () => {
 
+    const lastPath = localStorage.getItem('lastPath') || '/';
+
     login( 'Rasec Camacho' );
 
-    navigate('/', {
+    navigate(lastPath, {
       replace: true,
     });
   }
